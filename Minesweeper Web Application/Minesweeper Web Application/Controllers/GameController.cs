3e5b06471f595ba5abe18571053f9d0c51ded476
Vinson918 <vm84039@gmail.com>
2020-09-08 09:48:56 -0700

    merge

--- Minesweeper Web Application/Minesweeper Web Application/Controllers/GameController.cs
+++ Minesweeper Web Application/Minesweeper Web Application/Controllers/GameController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using System.IO;
 using System.Diagnostics;
 using System.Linq.Expressions;
+using Minesweeper_Web_Application.Services.Business;
 
 namespace Minesweeper_Web_Application.Controllers
 {
@@ -20,18 +21,25 @@ namespace Minesweeper_Web_Application.Controllers
         public static int safeSquares = (row * col - numofBombs);
         public static GameBoardModel board = null;
         public static List<GameSquareModel> squares = null;
+        public static DateTime startTime;
+
         public ActionResult Index()
         {
+            startTime = DateTime.Now;
             squares = new List<GameSquareModel>();
             board = new GameBoardModel(squares, row, col);
             board.PlaceBombs(squares, numofBombs);
             board.BombToString(squares);
+<<<<<<< .merge_file_a03584
             return View("Game", squares);
+=======
+            Debug.WriteLine(startTime);
+            return View("Game");
+>>>>>>> .merge_file_a05104
         }
         [HttpPost]
         public ActionResult OnButtonClick(string mine)
         {
-
             int value = Int32.Parse(mine);
             int index = value - 1;
             int r = index / row;
@@ -60,6 +68,16 @@ namespace Minesweeper_Web_Application.Controllers
 
             if(squaresRemaining == numofBombs)
             {
+                TimeSpan elapsed = (DateTime.Now - startTime);
+                decimal finalTime = Math.Round((decimal)elapsed.TotalSeconds, 2);
+
+                Debug.WriteLine(String.Format("{0} start time, {1] end time", startTime, DateTime.Now));
+                Debug.WriteLine("Difference in time.");
+                Debug.WriteLine(String.Format("{0} days, {1} hours, {2} minutes, {3} seconds", elapsed.Days, elapsed.Hours, elapsed.Minutes, elapsed.Seconds));
+
+                LeaderBoardService service = new LeaderBoardService();
+                service.InsertHighScore(UserManagement.Instance._loggedUser, finalTime);
+
                 ViewBag.squares = squares;
                 return View("Winner");
             }
@@ -67,5 +85,13 @@ namespace Minesweeper_Web_Application.Controllers
             return PartialView("_UpdateGame", squares);
         }
 
+<<<<<<< .merge_file_a03584
+=======
+        //Added this so we have it to post a flag on a right-click of an open space
+        public ActionResult OnButtonRightClick(string flagSpace)
+        {
+            return View("Game");
+        }
+>>>>>>> .merge_file_a05104
     }
 }
